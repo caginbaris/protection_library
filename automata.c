db8147f168f7e1764a1cbeb596b5608f2b9248bf
Viva-PC\Viva <caginbaris@gmail.com>
2019-10-27 20:53:41 +0200

    minor changes and revs on automate

--- automata.c
+++ automata.c
@@ -141,7 +141,47 @@ uint8_t on_off_delay2( uint8_t input,  uint8_t mem, uint32_t onSample,uint32_t o
 }
 
 
+void pulseGen(uint32_t prd, uint32_t duty,uint8_t EN,uint8_t* out,uint8_t* latch,uint32_t* prdCount){
+
+
+if(EN==1 && *latch==0){*out=1;*latch=1;}
+
+if(*latch==1){(*prdCount)=(*prdCount)+1;}
+
+if((*prdCount)>duty){*out=0;}
+
+if((*prdCount)==prd){*latch=0;*prdCount=0;}
+
+}
+
+
+
+uint32_t fToggle(uint32_t out,uint32_t sample, uint32_t* tCount){
+
+(*tCount)++;
+
+if(*tCount==sample){out^=1;*tCount=0;}
+
+return out;
+
+}
+
+
+
+
+
+
 void f_limiter(float* parameter,float downLimit,float upLimit){
+
+
+	if((*parameter)<downLimit){*parameter=downLimit;}
+	if((*parameter)>  upLimit){*parameter=  upLimit;}
+
+
+}
+
+
+void d_limiter(double* parameter,double downLimit,double upLimit){
 
 
 	if((*parameter)<downLimit){*parameter=downLimit;}
